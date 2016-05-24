ea1bbf466355c9be70815a006cf916c7e22b869f
Josh Hubbard <jhubbardsf@gmail.com>
2016-05-24 00:10:17 -0400

    Changed the twitter service IP to local in application.rb and to the external service in production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Production twitter service
+  config.twitter_service = 'http://107.170.87.119'
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
