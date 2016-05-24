e6d59efdeaf9dedca3765e8a65903b7e0f030414
Josh Hubbard <jhubbardsf@gmail.com>
2016-05-23 20:33:15 -0400

    Changed update time to one hour and added secret sample.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,6 @@ module ApplicationHelper
     minutes_ago = ((Time.now - Time.parse(tweets['tweets'][0]['create_date']))/60).to_i
     result = raw("<h1><div class='twitter-headline'>Recent Tweets for \##{topic}</div></h1>")
     result += raw("<h3 class='twitter-headline'>Results from #{pluralize(minutes_ago, 'minute')} ago.</h3>")
-    result += raw("<h3 class='twitter-headline'>New results in #{pluralize((10 - minutes_ago), 'minute')}.</h3>")
+    result += raw("<h3 class='twitter-headline'>New results in #{pluralize((60 - minutes_ago), 'minute')}.</h3>")
   end
 end
